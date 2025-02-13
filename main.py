3979bde57967df5058d33a414163ce36e498c9ab
coderhanzla@gmail.com <coderhanzla@gmail.com>
2025-02-13 21:27:12 +0530

    2EqualNumbers

---
+++ main.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter first number to compare: "))
+number2 = int(input("Enter second number to compare: "))
+
+if((number1 ^ number2)== 0):
+    print("numbers are equal")
+
+else:
+    print("both numbers are not equal")
